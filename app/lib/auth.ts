117b8d4f93aa387e7fec18977ac3118da61e8b51
Mark Watson <mwatson@prosaris.ca>
2023-06-11 09:51:32 -0300

    change redirect callback per nextauth docs

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -92,8 +92,12 @@ export const authOptions: NextAuthOptions = {
 
       return session;
     },
-    redirect({ url, baseUrl }) {
-      return '/dashboard';
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
     },
   },
 };
