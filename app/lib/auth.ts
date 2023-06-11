a86f609f6de20bdeda496659664e410189e75c87
Mark Watson <mwatson@prosaris.ca>
2023-06-11 09:54:50 -0300

    redirect to dashboard

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -93,11 +93,7 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
     async redirect({ url, baseUrl }) {
-      // Allows relative callback URLs
-      if (url.startsWith("/")) return `${baseUrl}${url}`
-      // Allows callback URLs on the same origin
-      else if (new URL(url).origin === baseUrl) return url
-      return baseUrl
+      return Promise.resolve('/dashboard')
     },
   },
 };
