977eb20eccd5817097c9242dcf4ac211def8da89
Mark Watson <mwatson@prosaris.ca>
2024-01-06 14:53:50 -0400

    add comment explaining cron route

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,6 +1,9 @@
 import { prisma } from '@/db/prismaClient';
 import { NextResponse } from 'next/server';
 
+// This route is for a cron job that is used to attempt ...
+// ... to keep the planetscale database awake. It goes to ...
+// ... sleep after 7 days of inactivity.
 export async function GET() {
   console.log('cron job is running');
 
